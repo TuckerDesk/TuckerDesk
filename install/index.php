9f8b16d6f030dd0db78660258b4a8b317844a3f0
Riverside Rocks <trent@riverside.rocks>
2020-07-30 18:42:09 -0400

    Added form

--- install/index.php
+++ install/index.php
@@ -7,6 +7,13 @@
         <center>
             <br>
             <h1>Install - TuckerDesk</h1>
+            <form action="run.php">
+                <label for="host">MySQL Host:</label><br>
+                <input type="text" id="host" name="host" value="localhost"><br>
+                <label for="user">Last name:</label><br>
+                <input type="text" id="user" name="user" value="root"><br><br>
+                <input type="submit" value="Submit">
+            </form>
         </center>
     </body>
 
