c9bf97452a4da43bb5ac6ff1f09a82e9ee27a631
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-07-30 16:11:06 -0400

    Create the home file.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+
+(c) Copyright 2020 Riverside Rocks and TuckerDesk Authors
+This code is under the Apache 2.0 License
+See the "LICENSE" file for more details
+
+*/
+
+
+// Check if installation file exists.
+
+if(file_exists("tuckerconf.php"){
+  // Do nothing, the configuration file has been added and the installation has already been complete.
+}else{
+  header("Location /install/index.php");
+  die();
+}
+
+?>
