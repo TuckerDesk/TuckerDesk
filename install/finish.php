f7ad00899cc89fa3fd52d516804b72580cd883f7
Riverside Rocks <trent@riverside.rocks>
2020-07-31 18:11:26 -0400

    Update finish.php

--- install/finish.php
+++ install/finish.php
@@ -3,5 +3,5 @@ require 'checkfile.php';
 checkConf();
 
 $myfile = fopen("/data/host.txt", "r") or die("Something went wrong...");
-echo fread($myfile,filesize("webdictionary.txt"));
+echo fread($myfile,filesize("/data/host.txt"));
 fclose($myfile);
