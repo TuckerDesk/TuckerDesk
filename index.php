39bc969920b8788c5e79f973d70fe3b10f8c5ca2
Riverside Rocks <trent@riverside.rocks>
2020-07-31 11:37:43 -0400

    Added >

--- index.php
+++ index.php
@@ -12,6 +12,6 @@ See the "LICENSE" file for more details
 // Check if installation file exists.
 
 if(!file_exists("tuckerconf.php")){
-  echo "<script src='redirect.js' /><body onload='setLocation();'";
+  echo "<script src='redirect.js' /><body onload='setLocation();'>";
 }
 ?>
