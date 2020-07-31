a6642c6fe60b03929905297844a0ee0ef9ff7e1d
Riverside Rocks <trent@riverside.rocks>
2020-07-31 18:18:55 -0400

    Build end stage.

--- install/finish.php
+++ install/finish.php
@@ -2,6 +2,19 @@
 require 'checkfile.php';
 checkConf();
 
-$myfile = fopen("/data/host.txt", "r") or die("Something went wrong...");
-echo fread($myfile,filesize("/data/host.txt"));
-fclose($myfile);
+if(!isset($_COOKIE['host'])){
+    echo "Something went wrong, please make sure cookies are allowed for this site.";
+}
+
+$host = $_COOKIE['host'];
+$username = $_COOKIE['username'];
+$password = $_COOKIE['password'];
+$database = $_COOKIE['database'];
+
+$conn = new mysqli($host, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code><br><br><button onclick='window.history.back();'>Go Back</button><div display='none'>");
+}
+echo "<h2>Your installation is all done. Click below to start adding articles.</h2>";
--- install/run.php
+++ install/run.php
@@ -26,31 +26,12 @@ if ($conn->connect_error) {
   die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code><br><br><button onclick='window.history.back();'>Go Back</button><div display='none'>");
 }
 echo "<h2>All set up! We are now able to work with MySQL. You will need to enter in a few more details.</h2>";
-echo "<i>Important: You MUST let all folders in this directable be writeable by TuckerDesk! Bad things will happen if you do not.</i>";
 
-/*
+setcookie("host", $host, time() + (86400 * 30), "/");
+setcookie("username", $username, time() + (86400 * 30), "/");
+setcookie("password", $password, time() + (86400 * 30), "/");
+setcookie("database", $database, time() + (86400 * 30), "/");
 
-This next step puts mysql details into text files.
-
-Don't panic! The data directory is not avalible on the inter
-
-You must grant write access to this directory.
-
-
-*/
-
-exec("cd .. && cd data");
-exec("${host} > host.txt");
-exec("${username} > username.txt");
-exec("${password} > password.txt");
-exec("${database} > database.txt");
-
-/*
-
-Keep in mind that if these these are not made VERY BAD THINGS WILL HAPPEN!
-(in other words, the program will be unable to connect to the database, none of the login/setup details will be saved...)
-
-*/
 
 
 ?>
