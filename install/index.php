a2943d65575b1265bc677a8b75b8b1145610010c
Riverside Rocks <trent@riverside.rocks>
2020-07-31 10:51:06 -0400

    Add more to install screen

--- install/index.php
+++ install/index.php
@@ -7,11 +7,16 @@
         <center>
             <br>
             <h1>Install - TuckerDesk</h1>
+            <h2>Let get started by setting up the database</h2>
             <form action="run.php">
                 <label for="host">MySQL Host:</label><br>
                 <input type="text" id="host" name="host" value="localhost"><br>
-                <label for="user">Last name:</label><br>
+                <label for="user">MySQL Username:</label><br>
                 <input type="text" id="user" name="user" value="root"><br><br>
+                <label for="pass">MySQL Password:</label><br>
+                <input type="password" id="pass" name="pass"><br><br>
+                <label for="data">MySQL Database:</label><br>
+                <input type="text" id="data" name="data" value="tuckerdesk"><br><br>
                 <input type="submit" value="Submit">
             </form>
         </center>
