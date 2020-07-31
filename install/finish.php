72189487e9a294a2f447588abc268fbcb582840c
Riverside Rocks <trent@riverside.rocks>
2020-07-31 18:09:41 -0400

    Try test #1
    Add a /

--- install/finish.php
+++ install/finish.php
@@ -2,6 +2,6 @@
 require 'checkfile.php';
 checkConf();
 
-$myfile = fopen("data/host.txt", "r") or die("Something went wrong...");
+$myfile = fopen("/data/host.txt", "r") or die("Something went wrong...");
 echo fread($myfile,filesize("webdictionary.txt"));
 fclose($myfile);
