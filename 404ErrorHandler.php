0c53e9e533a96b4f01d68efb0f7d67c2dd16707d
Riverside Rocks <trent@riverside.rocks>
2020-07-31 16:04:17 -0400

    Update 404ErrorHandler.php

--- 404ErrorHandler.php
+++ 404ErrorHandler.php
@@ -1,3 +1,11 @@
 <?php
-header("Location: /");
-die();
+
+// This is the 404 Handling Page
+// It checks if the tuckerconf file exisits, then redirects you to the homepage if you don't.
+
+if(!file_exists("tuckerconf.php")){
+    header("Location: /");
+    die();
+}else{
+    echo "<center><h1>404 - Not Found</h1></center>";
+}
