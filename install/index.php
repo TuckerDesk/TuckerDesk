fe58a36a367dcb912811981d1cc450d950720aad
Riverside Rocks <trent@riverside.rocks>
2020-07-31 11:42:44 -0400

    Fixed missing .

--- install/index.php
+++ install/index.php
@@ -23,7 +23,7 @@
             $ver = phpversion();
             $php = substr($ver,0,6);
             if($php > 7){
-                echo "<h3>Good, it looks like you are running " . $php "</h3>";
+                echo "<h3>Good, it looks like you are running " . $php . "</h3>";
             }else{
                 echo "<h3 class='error'>Your php version, " . $php . " is outdated. Consider upgrading.</h3>";
             }
