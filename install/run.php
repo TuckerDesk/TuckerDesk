3af4856a038b7114f54bbca5582dd1bd9f4442af
Riverside Rocks <trent@riverside.rocks>
2020-07-31 13:02:03 -0400

    Added form to run page

--- install/run.php
+++ install/run.php
@@ -19,6 +19,14 @@ $conn = new mysqli($host, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-  die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code><br><br><button onclick='window.history.back();'>Go Back</button>");
+  die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code><br><br><button onclick='window.history.back();'>Go Back</button><div display='none'>");
 }
 echo "<h2>All set up! We are now able to work with MySQL. You will need to enter in a few more details.</h2>";
+?>
+<form action="final.php" method="post">
+                <label for="name">Support Center Name:</label><br>
+                <input type="text" id="name" name="name" value="My Support Center"><br>
+                <label for="url">Logo URL:</label><br>
+                <input type="text" id="url" name="url" value="http://example.com/logo.jpg"><br><br>
+                <input type="submit" value="Submit">
+            </form>
