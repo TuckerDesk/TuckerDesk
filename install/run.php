554052bddeeb16d31ef5839c5224cf8e1fd24d0a
Riverside Rocks <trent@riverside.rocks>
2020-07-31 11:56:25 -0400

    Added return button

--- install/run.php
+++ install/run.php
@@ -19,6 +19,6 @@ $conn = new mysqli($host, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-  die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code>");
+  die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code><br><br><button onclick='window.history.back();'>Go Back</button>");
 }
 echo "<h2>All set up! We are now able to work with MySQL. You will need to enter in a few more details.</h2>";
