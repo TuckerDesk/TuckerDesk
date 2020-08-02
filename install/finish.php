13658d01a0386c87ecadb5482ef01e68b0b73826
Riverside Rocks <trent@riverside.rocks>
2020-08-02 09:05:54 -0400

    Add comma

--- install/finish.php
+++ install/finish.php
@@ -36,7 +36,7 @@ if ($conn->connect_error) {
   die("<h2>Sorry, the connection failed. MySQL Errors:</h2><br><code> " . $conn->connect_error . "</code><br><br><button onclick='window.history.back();'>Go Back</button><div display='none'>");
 }
 
-$sql = "source create.sql;";
+$sql = "source create.sql";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table created.";
