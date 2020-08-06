570b6b7de99afb71123e6aaad6b40e67cb17fcc0
Riverside Rocks <trent@riverside.rocks>
2020-08-06 09:57:41 -0400

    Begin home config

--- index.php
+++ index.php
@@ -25,15 +25,32 @@ if($toggle = "false"){ // Checks that the server is in production mode
     if(!file_exists("tuckerconf.php")){
       echo "<script>" . $code . "</script>";
     }else{
+
+      /*
+
+      This is the code to run when the server passes the production mode check
+      and the tuckerconf.php file exists
+
+      */
+
       require "tuckerconf.php";
-      echo "<script>></script>";
+      echo "<script src='js/prodmode.js'></script>";
       $conn = new mysqli($host, $username, $password, $database); // Create a connection to the database with the stored values in tuckerconf.php
       if ($conn->connect_error) {
         die("<h2>Sorry, something went wrong. Please report error 2 and this code to our <a href='https://github.com/TuckerDesk/TuckerDesk/issues' />Github</a>:</h2><br><code> " . $conn->connect_error . "</code><br>");
       }
 
+      $sql = "SELECT title FROM ${database}";
+
+      /*
+
+      Work on this part later...
+
+      */
+
     }
 else{
     // Do not redirect, the server is in non-production mode.
+    echo "<script src='js/devmode.js'></script>";
 }
 ?>
