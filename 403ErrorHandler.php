da4fe4ad2e1c00ef2ecff7b6f3c04f4c245c9d08
Riverside Rocks <trent@riverside.rocks>
2020-08-06 09:31:14 -0400

    Added 403 errer

--- 403ErrorHandler.php
+++ 403ErrorHandler.php
@@ -1,11 +1,11 @@
 <?php
 
-// This is the 404 Handling Page
+// This is the 403 Handling Page
 // It checks if the tuckerconf.php file exisits, then redirects you to the homepage if you don't.
 
 if(!file_exists("tuckerconf.php")){
     header("Location: /");
     die();
 }else{
-    echo "<center><h1>404 - Not Found</h1></center>";
+    echo "<center><h1>403 - Forbidden</h1></center>";
 }
