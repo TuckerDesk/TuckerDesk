2d3044d2c5936b847dc199c09faabf06ef5a8f2f
Riverside Rocks <trent@riverside.rocks>
2020-08-09 18:25:01 -0400

    Table SQL

--- install/create.sql
+++ install/create.sql
@@ -6,12 +6,3 @@ CREATE USER 'tuckerdesk'@'localhost' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON * . * TO 'tuckerdesk'@'localhost';
 
 FLUSH PRIVILEGES;
-
--- This creates a table for the details of the
--- support center.
-
-CREATE TABLE IF NOT EXISTS details (
-  title VARCHAR(255) NOT NULL,
-  logo VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-)  ENGINE=INNODB;
